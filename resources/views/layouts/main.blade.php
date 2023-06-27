92c4d9b4003ff670e50704a02d3778abf79b7bfc
petrafellicia <112378580+petrafellicia@users.noreply.github.com>
2023-06-27 12:37:04 +0700

    first

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,15 @@
+<body>
+    <head>
+
+<!-- Bootstrap CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
+integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDiYZCxYbOOl7+AMvyTG2X"
+crossorigin="anonymous">
+
+<!-- My Style -->
+<link rel="stylesheet" href="/css/style.css">
+
+<title>Arsip-Polda Blog | {{$title}}</title>
+
+    </head>
+</body>
