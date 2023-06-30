d68b9edccf942d2a8623ba26418c74779c5d70d9
petrafellicia <112378580+petrafellicia@users.noreply.github.com>
2023-06-30 22:11:40 +0700

    coba buat login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -1,12 +1,22 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+// use Illuminate\Foundation\User\AuthenticatesUsers;
 
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
     //
+    use AuthenticatesUsers;
+
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+
     public function index(){
         return view('login.index', [
             'title' => 'Login'
