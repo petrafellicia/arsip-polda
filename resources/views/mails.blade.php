65994839b567711f5cc3e24589098ce8f1d29f51
Alfrina29 <alfrina.g.p@gmail.com>
2023-07-17 08:37:35 +0700

    Update mails.blade.php

--- resources/views/mails.blade.php
+++ resources/views/mails.blade.php
@@ -5,7 +5,7 @@
 @foreach ($mails as $mail)
 <button type="button" class="btn btn-primary"
         style=" width:181px; height:50px; border-radius:20px; --bs-btn-padding-y: .50rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem; color: white">
-        <a href="/mails/{{ $mail["slug"] }}"> {{ $mail["title"] }}</a>
+        <a href="/mails/{{ $mail["slug"] }}" > {{ $mail["title"] }}</a>
 </button>
 <!-- <button type="button" class="btn btn-primary btn-lg"></button> -->
   <!-- <h5>by : {{  $mail["author"] }}</h5>
