a846e0e7a51b16e98e8f9822747b38d987f5717d
Alfrina29 <alfrina.g.p@gmail.com>
2023-07-20 07:49:16 +0700

    modified:   resources/views/masuk.blade.php

--- resources/views/masuk.blade.php
+++ resources/views/masuk.blade.php
@@ -93,156 +93,156 @@
   <body>
   <h2 style="margin-top: 5px; margin-left:400px;"><b>Input-Update Data Surat Masuk</b></h2>
   <form method ="post" action="">
-    
+     
   <fieldset>
-  <legend>| Data Surat Masuk |</legend>
-  <table>
-    <tr>
-         <td>Nomor Agenda </td>
-         <td>: <input type="text" name="nomoragenda" size ="40" placeholder=
-          "isi nomor agenda" required></td>
-    </tr>
-    <tr>
-         <td>Nomor Surat </td>
-         <td>: <input type="text" name="nomorsurat" size ="40" placeholder=
-          "isi nomor surat" required></td>
-    </tr>
-    <tr>
-        <td>Jenis Surat </td>
-        <td>: <select name="type">
-            <option>Surat Biasa</option>
-            <option>Nota Dinas</option>
-            <option>Telegram</option>
-            <option>Sprin</option>
-            <option>Surat Izin</option>
-            <option>Surat Rahasia</option>
-        </select>
-        </td>
-    </tr>
-    <tr>
-        <td>Asal Surat</td>
-        <td>: <input type="text" name="asalsurat" size ="40"
-         placeholder="isi asal surat lengkap" required></td>
-    </tr>
-    <tr>
-        <td>Perihal </td>
-        <td>: <input type="radio" name="jk" value="majalah"
-         required>Majalah
-
-        <input type="radio" name="jk" value="tribut"
-         required>Tribut
-        
-         <input type="radio" name="jk" value="news"
-         required>News</td>
-    </tr>
-    <tr>
-        <td>KKA </td>
-        <td>: <select name="kka">
-            <option>KEP</option>
-            <option>BIN</option>
-            <option>OPS</option>
-            <option>REN</option>
-            <option>LOG</option>
-            <option>HUM</option>
-            <option>HUK</option>
-            <option>TIK</option>
-            <option>TUK</option>
-            <option>WAS</option>
-            <option>KEU</option>
-            <option>DIK</option>
-            <option>PAM</option>
-            <option>YAN</option>
-            <option>KES</option>
-            <option>RES</option>
-        </select>
-        </td>
-    </tr>
-    <tr>
-        <td>Tanggal Surat</td>
-        <td>: <input type="date" name="tanggalsurat" size ="40"
-         placeholder="isi tanggal" required></td>
-    </tr>
-    <tr>
-         <td>Waktu Terima Surat </td>
-         <td>: <input type="text" name="waktu" size ="40" placeholder=
-          "isi waktu terima surat" required></td>
-    </tr>
-    <tr>
-        <td>Disposisi Kepada </td>
-        <td>: <input type="checkbox" name="jk" value="tekkom"
-         required>Ksbd. Tekkom
-
-        <input type="checkbox" name="jk" value="tekinfo"
-         required>Ksbd. Tekinfo
-        
-         <input type="checkbox" name="jk" value="renmin"
-         required>Ksbg. Renmin
-
-         <input type="checkbox" name="jk" value="other"
-         required>Other</td>
-    </tr>
-    <tr>
-        <td>Distribusi </td>
-        <td>: <select name="distribusi">
-            <option>Mabes POLRI</option>
-            <option>Kapolda DIY</option>
-            <option>Wakapolda DIY</option>
-            <option>Irwasda</option>
-            <option>Karo Ops</option>
-            <option>Karo Rena</option>
-            <option>Karo SDM</option>
-            <option>Karo Log</option>
-            <option>Dirintelkom</option>
-            <option>Dirreskrimum</option>
-            <option>Dirreskrimsus</option>
-            <option>Dirresnarkoba</option>
-            <option>Kabid Propam</option>
-            <option>Kabid Humas</option>
-            <option>Kabid TIK</option>
-            <option>Kabid Dokkes</option>
-            <option>Kabid Kum</option>
-            <option>Kabid Keu</option>
-            <option>Dittahti</option>
-            <option>Kayanma</option>
-            <option>Koorspripim</option>
-            <option>Karumkit Bhayangkara</option>
-            <option>KA SPKT Polda DIY</option>
-            <option>KA SPN</option>
-            <option>Dansat Brimob</option>
-            <option>Polrestu YKA</option>
-            <option>Polrestu Sleman</option>
-            <option>Polres Bantul</option>
-            <option>Polres KLP</option>
-            <option>Polres ONK</option>
-            <option>Instansi Luar Polda DIY</option>
-        </select>
-        </td>
-    </tr>
-    <tr>
-        <td>Isi Disposisi </td>
-        <td>: <input type="checkbox" name="jk" value="tinjau"
-         required>Tinjau
-
-        <input type="checkbox" name="jk" value="accept"
-         required>Accepted
-        
-         <input type="checkbox" name="jk" value="koordinasi"
-         required>Koordinasi</td>
-    </tr>
-    <tr>
-        <td>Keterangan </td>
-        <td>: <input type="text" name="keterangan" size ="40"
-         placeholder="isi keterangan"></td>
-    </tr>
-    <tr>
-        <td>Scan Surat </td>
-        <td>: <input type="file" name="surat" required> </td>
-    </tr>
-    <tr>
-        <td><input type="submit" name="submit" value="Submit"></td>
-    </tr>
-   </table>
-  </fieldset>
+    <legend>| Data Surat Masuk |</legend>
+    <table>
+      <tr>
+           <td>Nomor Agenda </td>
+           <td>: <input type="text" name="nomoragenda" size ="40" placeholder=
+            "isi nomor agenda" required></td>
+      </tr>
+      <tr>
+           <td>Nomor Surat </td>
+           <td>: <input type="text" name="nomorsurat" size ="40" placeholder=
+            "isi nomor surat" required></td>
+      </tr>
+      <tr>
+          <td>Jenis Surat </td>
+          <td>: <select name="type">
+              <option>Surat Biasa</option>
+              <option>Nota Dinas</option>
+              <option>Telegram</option>
+              <option>Sprin</option>
+              <option>Surat Izin</option>
+              <option>Surat Rahasia</option>
+          </select>
+          </td>
+      </tr>
+      <tr>
+          <td>Asal Surat</td>
+          <td>: <input type="text" name="asalsurat" size ="40"
+           placeholder="isi asal surat lengkap" required></td>
+      </tr>
+      <tr>
+          <td>Perihal </td>
+          <td>: <input type="radio" name="jk" value="majalah"
+           required>Majalah
+  
+          <input type="radio" name="jk" value="tribut"
+           required>Tribut
+          
+           <input type="radio" name="jk" value="news"
+           required>News</td>
+      </tr>
+      <tr>
+          <td>KKA </td>
+          <td>: <select name="kka">
+              <option>KEP</option>
+              <option>BIN</option>
+              <option>OPS</option>
+              <option>REN</option>
+              <option>LOG</option>
+              <option>HUM</option>
+              <option>HUK</option>
+              <option>TIK</option>
+              <option>TUK</option>
+              <option>WAS</option>
+              <option>KEU</option>
+              <option>DIK</option>
+              <option>PAM</option>
+              <option>YAN</option>
+              <option>KES</option>
+              <option>RES</option>
+          </select>
+          </td>
+      </tr>
+      <tr>
+          <td>Tanggal Surat</td>
+          <td>: <input type="date" name="tanggalsurat" size ="40"
+           placeholder="isi tanggal" required></td>
+      </tr>
+      <tr>
+           <td>Waktu Terima Surat </td>
+           <td>: <input type="text" name="waktu" size ="40" placeholder=
+            "isi waktu terima surat" required></td>
+      </tr>
+      <tr>
+          <td>Disposisi Kepada </td>
+          <td>: <input type="checkbox" name="jk" value="tekkom"
+           required>Ksbd. Tekkom
+  
+          <input type="checkbox" name="jk" value="tekinfo"
+           required>Ksbd. Tekinfo
+          
+           <input type="checkbox" name="jk" value="renmin"
+           required>Ksbg. Renmin
+  
+           <input type="checkbox" name="jk" value="other"
+           required>Other</td>
+      </tr>
+      <tr>
+          <td>Distribusi </td>
+          <td>: <select name="distribusi">
+              <option>Mabes POLRI</option>
+              <option>Kapolda DIY</option>
+              <option>Wakapolda DIY</option>
+              <option>Irwasda</option>
+              <option>Karo Ops</option>
+              <option>Karo Rena</option>
+              <option>Karo SDM</option>
+              <option>Karo Log</option>
+              <option>Dirintelkom</option>
+              <option>Dirreskrimum</option>
+              <option>Dirreskrimsus</option>
+              <option>Dirresnarkoba</option>
+              <option>Kabid Propam</option>
+              <option>Kabid Humas</option>
+              <option>Kabid TIK</option>
+              <option>Kabid Dokkes</option>
+              <option>Kabid Kum</option>
+              <option>Kabid Keu</option>
+              <option>Dittahti</option>
+              <option>Kayanma</option>
+              <option>Koorspripim</option>
+              <option>Karumkit Bhayangkara</option>
+              <option>KA SPKT Polda DIY</option>
+              <option>KA SPN</option>
+              <option>Dansat Brimob</option>
+              <option>Polrestu YKA</option>
+              <option>Polrestu Sleman</option>
+              <option>Polres Bantul</option>
+              <option>Polres KLP</option>
+              <option>Polres ONK</option>
+              <option>Instansi Luar Polda DIY</option>
+          </select>
+          </td>
+      </tr>
+      <tr>
+          <td>Isi Disposisi </td>
+          <td>: <input type="radio" name="jk" value="tinjau"
+           required>Tinjau
+  
+          <input type="radio" name="jk" value="accept"
+           required>Accepted
+          
+           <input type="radio" name="jk" value="koordinasi"
+           required>Koordinasi</td>
+      </tr>
+      <tr>
+          <td>Keterangan </td>
+          <td>: <input type="text" name="keterangan" size ="40"
+           placeholder="isi keterangan"></td>
+      </tr>
+      <tr>
+          <td>Scan Surat </td>
+          <td>: <input type="file" name="surat" required> </td>
+      </tr>
+      <tr>
+          <td><input type="submit" name="submit" value="Submit"></td>
+      </tr>
+     </table>
+    </fieldset>
  </form>
     </body>
     </html>
