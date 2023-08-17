8c962da2162d4a9d39b83d54786fed28600f964c
Alfrina29 <alfrina.g.p@gmail.com>
2023-08-17 23:58:20 +0700

    modified:   resources/views/suratkeluar.blade.php

--- resources/views/suratkeluar.blade.php
+++ resources/views/suratkeluar.blade.php
@@ -7,64 +7,71 @@
     <div class="container-fluid">
         <a href="/mails">Back to Mails</a>
         <button type="button"
-            style=" width:150px; float:right; height:40px; border-radius:26.5px; --bs-btn-padding-y: .50rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem; border:none; outline:none; padding:8px; cursor:pointer; background:#216588;">
-            <a style="color:white; justify-content:center; text-decoration:none;" href="/keluar">Tambah Surat</a></button>
-        <form action="/daftar-surat-keluar/search" method="GET" class="d-flex justify-content-end" style="margin-top:30px; margin-bottom:10px;"
-            role="search">
-            <input class="form-control w-25 me-2" type="search" name="search" value="{{ request('search') }}"
+            style=" float:right; width:150px; height:40px; border-radius:26.5px; --bs-btn-padding-y: .50rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem; border:none; outline:none; padding:8px; cursor:pointer; background:#216588;">
+            <a style="color:white; justify-content:center; text-decoration:none;" href="/masuk">Tambah Surat</a></button>
+        <form action="/daftar-surat-masuk/search" method="GET" class="d-flex justify-content-end"
+            style="margin-top:30px; margin-bottom:10px;" role="search">
+
+            <input class="form-control w-25 me-2" name="search" type="search" value="{{ request('search') }}"
                 placeholder="Search" aria-label="Search">
             <button class="btn btn-outline-success" type="submit">Search</button>
+
         </form>
     </div>
     @if (!empty($message))
         <p>{{ $message }}</p>
     @elseif (!empty($data))
-    <table class="table-responsive" style="text-align:center;">
-        <thead>
-            <tr>
-                <th scope="col">Nomor Agenda</th>
-                <th scope="col">Nomor Surat</th>
-                <th scope="col">Jenis Surat</th>
-                <th scope="col">Surat dari</th>
-                <th scope="col">Perihal</th>
-                <th scope="col">KKA</th>
-                <th scope="col">Dasar Pembuatan Surat</th>
-                <th scope="col">Tanggal Surat</th>
-                <th scope="col">Jam Diterima Surat</th>
-                <th scope="col">Disposisi kepada</th>
-                <th scope="col">Distribusi</th>
-                <th scope="col">Isi Disposisi</th>
-                <th scope="col">Feedback</th>
-                <th scope="col">File</th>
-                <th scope="col">Aksi</th>
-            </tr>
-        </thead>
-        <tbody class="table-group-divider">
-            @foreach ($data as $row)
+        <table class="table" style="text-align:center; font-size:14px;">
+            <thead>
                 <tr>
-                    <th scope="row">{{ $row->no_agenda }}</th>
-                    <td>{{ $row->no_surat }}</td>
-                    <td>{{ $row->jenis_surat }}</td>
-                    <td>{{ $row->asal_surat }}</td>
-                    <td>{{ $row->perihal }}</td>
-                    <td>{{ $row->kka }}</td>
-                    <td>{{ $row->dasar_surat }}</td>
-                    <td>{{ date('d-m-Y', strtotime($row->tgl_surat)) }}</td>
-                    <td>{{ $row->jam_surat }}</td>
-                    <td>{{ $row->disposisi }}</td>
-                    <td>{{ $row->distribusi }}</td>
-                    <td>{{ $row->isi_disposisi }}</td>
-                    <td>{{ $row->feedback }}</td>
-                    <td>
-                        <a href="dokumensuratkeluar/{{ $row->file }}" class="btn btn-success">Download</a>
-                    </td>
-                    <td><a href="/tampilkandatakeluar/{{ $row->id }}" class="btn btn-primary">Edit</button></td>
-                    <td><a href="/deletekeluar/{{ $row->id }}" class="btn btn-danger">Delete</button></td>
+                    <th scope="col">Nomor Agenda</th>
+                    <th scope="col">Nomor Surat</th>
+                    <th scope="col">Jenis Surat</th>
+                    <th scope="col">Surat dari</th>
+                    <th scope="col">Perihal</th>
+                    <th scope="col">KKA</th>
+                    <th scope="col">Dasar Pembuatan Surat</th>
+                    <th scope="col">Tanggal Surat</th>
+                    <th scope="col">Jam Diterima Surat</th>
+                    <th scope="col">Disposisi kepada</th>
+                    <th scope="col">Distribusi</th>
+                    <th scope="col">Isi Disposisi</th>
+                    <th scope="col">Feedback</th>
+                    <th scope="col">File</th>
+                    <th scope="col">Aksi</th>
                 </tr>
-            @endforeach
-            @endif
-        </tbody>
+            </thead>
+            <tbody class="table-group-divider">
+
+
+                @foreach ($data as $row)
+                    <tr>
+                        <th scope="row">{{ $row->no_agenda }}</th>
+                        <td>{{ $row->no_surat }}</td>
+                        <td>{{ $row->jenis_surat }}</td>
+                        <td>{{ $row->asal_surat }}</td>
+                        <td>{{ $row->perihal }}</td>
+                        <td>{{ $row->kka }}</td>
+                        <td>{{ $row->dasar_surat }}</td>
+                        <td>{{ date('d-m-Y', strtotime($row->tgl_surat)) }}</td>
+                        <td>{{ $row->jam_surat }}</td>
+                        <td>{{ $row->disposisi }}</td>
+                        <td>{{ $row->distribusi }}</td>
+                        <td>{{ $row->isi_disposisi }}</td>
+                        <td>{{ $row->feedback }}</td>
+                        <td>
+                            <a href="dokumensuratkeluar/{{ $row->file }}" class="btn btn-success"
+                                style="font-size: 13px;">Download</a>
+                        </td>
+                        <td><a href="/tampilkandatakeluar/{{ $row->id }}" class="btn btn-primary"
+                            style="font-size: 13px;">Edit</button></td>
+                        <td><a href="/deletekeluar/{{ $row->id }}" class="btn btn-danger"
+                            style="font-size: 13px;">Delete</button></td>
+                    </tr>
+                @endforeach
+    @endif
+    </tbody>
     </table>
     @include('sweetalert::alert')
-     {{ $data->links() }}
+    {{ $data->links() }}
 @endsection
