b57349a124af0bd9a53a7daaa57e5744c0394a75
petrafellicia <112378580+petrafellicia@users.noreply.github.com>
2023-08-18 09:09:12 +0700

    naur

--- config/app.php
+++ config/app.php
@@ -182,7 +182,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         RealRashid\SweetAlert\SweetAlertServiceProvider::class,
-        Barryvdh\DomPDF\ServiceProvider::class,
+        
 
             /*
              * Package Service Providers...
@@ -196,6 +196,7 @@ return [
             // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Barryvdh\DomPDF\ServiceProvider::class,
 
     ],
 
@@ -213,7 +214,7 @@ return [
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
         'Alert' => RealRashid\SweetAlert\Facades\Alert::class,
-        
+
     ])->toArray(),
 
 ];
