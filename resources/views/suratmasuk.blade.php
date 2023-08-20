e627c8f88fb0dd8839753e587c2c9801d421a0dc
Alfrina29 <alfrina.g.p@gmail.com>
2023-08-20 22:45:05 +0700

    modified:   resources/views/suratmasuk.blade.php

--- resources/views/suratmasuk.blade.php
+++ resources/views/suratmasuk.blade.php
@@ -19,41 +19,20 @@
                     Surat</a>
                 <!-- <button button id="rekapButton" class="btn btn-info" style="margin-bottom: 1rem; margin-right:45rem;">Rekap Surat</button> -->
             </div>
-                    <input class="form-control w-25 me-2" name="search" type="search" value="{{ request('search') }}"
-                        placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit" style="margin-bottom:1rem; font-size:14px;">Search</button>       
-            
-            </form>
+            <input class="form-control w-25 me-2" name="search" type="search" value="{{ request('search') }}"
+                placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit"
+                style="margin-bottom:1rem; font-size:14px;">Search</button>
+
+        </form>
     </div>
     @if (!empty($pesan))
         <p>{{ $pesan }}</p>
     @elseif (!empty($data))
-    <div class="table-responsive">
-        <table class="table" style="text-align:center; font-size:14px; ">
-            <thead>
-                <tr style="text-wrap:nowrap;">
-                    <th scope="col">Nomor Agenda</th>
-                    <th scope="col">Nomor Surat</th>
-                    <th scope="col">Jenis Surat</th>
-                    <th scope="col">Surat dari</th>
-                    <th scope="col">Perihal</th>
-                    <th scope="col">KKA</th>
-                    <th scope="col">Tanggal Surat</th>
-                    <th scope="col">Jam Diterima </th>
-                    <th scope="col">Disposisi</th>
-                    <th scope="col">Distribusi</th>
-                    <th scope="col">Isi Disposisi</th>
-                    <th scope="col">Keterangan</th>
-                    <th scope="col">Dokumen</th>
-                    <th scope="col">Aksi</th>
-                </tr>
-            </thead>
-
-            <tbody class="table-group-divider">
-
-
-                @foreach ($data as $row)
-                    <tr>
+        <div class="table-responsive">
+            <table class="table" style="text-align:center; font-size:14px; ">
+                <thead>
+                    <tr style="text-wrap:nowrap;">
                         <th scope="col">Nomor Agenda</th>
                         <th scope="col">Nomor Surat</th>
                         <th scope="col">Jenis Surat</th>
@@ -70,6 +49,28 @@
                         <th scope="col">Aksi</th>
                     </tr>
                 </thead>
+
+                <tbody class="table-group-divider">
+
+
+                    @foreach ($data as $row)
+                        <tr>
+                            <th scope="col">Nomor Agenda</th>
+                            <th scope="col">Nomor Surat</th>
+                            <th scope="col">Jenis Surat</th>
+                            <th scope="col">Surat dari</th>
+                            <th scope="col">Perihal</th>
+                            <th scope="col">KKA</th>
+                            <th scope="col">Tanggal Surat</th>
+                            <th scope="col">Jam Diterima </th>
+                            <th scope="col">Disposisi</th>
+                            <th scope="col">Distribusi</th>
+                            <th scope="col">Isi Disposisi</th>
+                            <th scope="col">Keterangan</th>
+                            <th scope="col">Dokumen</th>
+                            <th scope="col">Aksi</th>
+                        </tr>
+                        </thead>
 
                 <tbody class="table-group-divider">
 
@@ -95,7 +96,7 @@
                             <td scope="row"><a href="/tampilkandatamasuk/{{ $row->id }}" class="btn btn-primary"
                                     style="font-size: 13px">Edit</td>
                             <!-- <td scope="row"><a href="/deletemasuk/{{ $row->id }}" class="btn btn-danger"
-                                    style="font-size: 13px">Delete -->
+                                                style="font-size: 13px">Delete -->
                             <td scope="row">
                                 <a href="#" class="btn btn-danger delete-button" data-id="{{ $row->id }}"
                                     style="font-size: 13px">Delete</a>
@@ -121,7 +122,7 @@
                     if (confirmDelete) {
                         var dataId = this.getAttribute("data-id");
                         window.location.href = '/deletefilemasuk/' +
-                        dataId; // Ganti dengan URL delete yang sesuai
+                            dataId; // Ganti dengan URL delete yang sesuai
                     }
                 });
             });
@@ -137,42 +138,42 @@
             });
         });
     </script>
-    <style>
-        .card {
-            display: none;
-            position: fixed;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-            background-color: white;
-            padding: 20px;
-            border-radius: 5px;
-            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.3);
-            z-index: 1000;
-        }
-    </style>
-    <div id="card" class="card">
-        <h4>Pilih Bulan untuk Rekap Surat</h4>
-        <form action="{{ route('exportpdfmasuk') }}" method="post">
-            @csrf
-            <label for="bulan">Pilih Bulan:</label>
-            <select name="bulan" id="bulan">
-                <option value="1">Januari</option>
-             <option value="02">Februari</option>
-            <option value="03">Maret</option>
-            <option value="04">April</option>
-            <option value="05">Mei</option>
-            <option value="06">Juni</option>
-            <option value="07">Juli</option>
-            <option value="08">Agustus</option>
-            <option value="09">September</option>
-            <option value="10">Oktober</option>
-            <option value="11">November</option>
-                            <option value="12">Desember</option>
-                            </select>
-                            <button type="submit" class="btn btn-primary">Generate PDF</button>
-        </form>
-    </div> -->
+                <style>
+                    .card {
+                        display: none;
+                        position: fixed;
+                        top: 50%;
+                        left: 50%;
+                        transform: translate(-50%, -50%);
+                        background-color: white;
+                        padding: 20px;
+                        border-radius: 5px;
+                        box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.3);
+                        z-index: 1000;
+                    }
+                </style>
+                <div id="card" class="card">
+                    <h4>Pilih Bulan untuk Rekap Surat</h4>
+                    <form action="{{ route('exportpdfmasuk') }}" method="post">
+                        @csrf
+                        <label for="bulan">Pilih Bulan:</label>
+                        <select name="bulan" id="bulan">
+                            <option value="1">Januari</option>
+                         <option value="02">Februari</option>
+                        <option value="03">Maret</option>
+                        <option value="04">April</option>
+                        <option value="05">Mei</option>
+                        <option value="06">Juni</option>
+                        <option value="07">Juli</option>
+                        <option value="08">Agustus</option>
+                        <option value="09">September</option>
+                        <option value="10">Oktober</option>
+                        <option value="11">November</option>
+                                        <option value="12">Desember</option>
+                                        </select>
+                                        <button type="submit" class="btn btn-primary">Generate PDF</button>
+                    </form>
+                </div> -->
 
     {{ $data->links() }}
 @endsection
