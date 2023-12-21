6473c738b4b2901bca829b2f4940aafe603f2cf9
Alfrina29 <alfrina.g.p@gmail.com>
2023-12-21 18:33:56 +0700

    new file:   app/Http/Controllers/UnitController.php 	new file:   app/Http/Requests/StoreUnitRequest.php 	new file:   app/Http/Requests/UpdateUnitRequest.php 	new file:   app/Models/Unit.php 	new file:   app/Policies/UnitPolicy.php 	new file:   database/factories/UnitFactory.php 	new file:   database/migrations/2023_12_21_090740_create_units_table.php 	new file:   database/seeders/UnitSeeder.php

---
+++ database/seeders/UnitSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Carbon;
+
+class UnitSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('units')->insert([
+            'id' => '1',
+            'nama_unit'=> 'BID TIK',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+    }
+}
